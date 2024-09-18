c0b5c5865821eab6378507905ebb8c450d02007d
madyanes <madyanes.dev@gmail.com>
2024-09-18 21:07:52 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Counter from './Counter'
+import './styles.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Compound Component Pattern</h1>
+      <Counter
+        iconIncrease='+'
+        iconDecrease='-'
+        label='My NOT so flexible counter'
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      />
+    </div>
+  )
+}
+
+export default App
